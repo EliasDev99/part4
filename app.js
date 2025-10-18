f9f5071863b44bed80bdf727055094885ed4e95a
unknown <elias.dev99@gmail.com>
2025-10-18 12:27:26 +0300

    Add 4.2 Blog List, step 2

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const { MONGODB_URI } = require('./utils/config.js')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs.js')
+
+logger.info('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connection to MongoDB:', error.message)
+    })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ utils/config.js
@@ -0,0 +1,12 @@
+const { error } = require('./logger.js')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3003
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+  error('⚠️  MONGODB_URI not set in environment variables')
+}
+
+module.exports = { MONGODB_URI, PORT }
